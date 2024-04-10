2f202878f30665aa5cff63d48e5405a475a3e647
Donat_Shergalis <donat_shergalis@epam.com>
2024-04-10 19:50:23 +0200

    Open Messages tab by default when opening a Topic
    (cherry picked from commit a1715d6ddc9e92f978e105c46f70cc297a18dd34)
    (cherry picked from commit 8f9dfd0dd28f3df8f719bfa42beee4ad3193c388)

--- frontend/src/components/Topics/List/TopicTitleCell.tsx
+++ frontend/src/components/Topics/List/TopicTitleCell.tsx
@@ -3,13 +3,16 @@ import { CellContext } from '@tanstack/react-table';
 import { Tag } from 'components/common/Tag/Tag.styled';
 import { Topic } from 'generated-sources';
 import { NavLink } from 'react-router-dom';
+import {
+  clusterTopicMessagesRelativePath,
+} from 'lib/paths';
 
 export const TopicTitleCell: React.FC<CellContext<Topic, unknown>> = ({
   row: { original },
 }) => {
   const { internal, name } = original;
   return (
-    <NavLink to={name} title={name}>
+    <NavLink to={name + '/' + clusterTopicMessagesRelativePath} title={name}>
       {internal && (
         <>
           <Tag color="gray">IN</Tag>
